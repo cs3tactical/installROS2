3cc9c25b76f227da6a4cbfdb0116f0492dc5d358
Ziv barcesat <barcesat@gmail.com>
2024-06-24 10:04:08 +0300

    update for humble

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # installROS2
-Scripts to install ROS2 (foxy) on the NVIDIA Jetson Development Kits
+Scripts to install ROS2 (humble) on the NVIDIA Jetson Development Kits
 
 This is a simple script to install ROS2 on the NVIDIA Jetson Development Kits.
 
@@ -9,22 +9,27 @@ In order to run the script:
 
 The script roughly follows the 'Install ROS From Source' procedures from:
 
-<blockquote>https://index.ros.org/doc/ros2/Installation/Foxy/Linux-Development-Setup/</blockquote>
+<blockquote>https://index.ros.org/doc/ros2/Installation/Humble/Linux-Development-Setup/</blockquote>
 
 Much of the code is taken from the dusty-nv Github repository jetson-containers. The dusty-nv jetson-containers should be used to create a Docker container for the ROS2 on the Jetson. For more information:
 
 <blockquote>
-Dockerfile.ros.foxy
+Dockerfile.ros.humble
 https://github.com/dusty-nv/jetson-containers
 </blockquote> 
 
 <h3>Notes</h3>
-Currently the NVIDIA Jetsons run Ubuntu 18.04. ROS2 foxy requires Ubuntu 20.04. However, by providing some of the 20.04 equivalents, it is possible to run ROS2 foxy on the Jetsons. This script provides workarounds. Note that this is not exhaustive, you may run into situations where there are gaps, and certain packages may have issues.
+Currently the NVIDIA Jetsons run Ubuntu 20.04. ROS2 humble requires Ubuntu 20.04. However, by providing some of the 20.04 equivalents, it is possible to run ROS2 humble on the Jetsons. This script provides workarounds. Note that this is not exhaustive, you may run into situations where there are gaps, and certain packages may have issues.
 
 <br><p>The file ~/.bashrc is modified using this script; You will want to make sure that the modifications match your expectations. For example, you may have a preference for having the install/setup.bash in another place (i.e. /root/.bashrc).
  
 
 <h3>Release Notes</h3>
+
+<b>June, 2024</b>
+* Changed to build ROS2 Humble instead of Foxy
+* Tested on JetPack 5.12 L4T 35.4.1
+* Tested on Jetson Orin Nano 8GB (with NVME)
 
 <b>January, 2021</b>
 * Tested on JetPack 4.5, L4T 32.5
--- installROS2.sh
+++ installROS2.sh
@@ -4,19 +4,25 @@
 # MIT License
 
 # Roughly follows the 'Install ROS From Source' procedures from:
-#   https://index.ros.org/doc/ros2/Installation/Foxy/Linux-Development-Setup/
+#   https://docs.ros.org/en/humble/Installation/Alternatives/Ubuntu-Development-Setup.html
 # mostly from: 
 #   Dockerfile.ros.foxy
 #   https://github.com/dusty-nv/jetson-containers
 # 
 
 ROS_PKG=ros_base
-ROS_DISTRO=foxy
+ROS_DISTRO=humble
 # Core ROS2 workspace - the "underlay"
 ROS_BUILD_ROOT=/opt/ros/${ROS_DISTRO}-src
 ROS_INSTALL_ROOT=/opt/ros/${ROS_DISTRO}
 
 locale  # check for UTF-8
+
+# Add the ROS 2 apt repository
+# You will need to add the ROS 2 apt repository to your system.
+# First ensure that the Ubuntu Universe repository is enabled.
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
 
 sudo apt update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
@@ -28,23 +34,29 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
 		curl \
 		wget \ 
-		gnupg2 \
+		gnupg \
 		lsb-release
 sudo rm -rf /var/lib/apt/lists/*
-    
-wget --no-check-certificate https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc 
-sudo apt-key add ros.asc
-sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list'
+
+# wget --no-check-certificate https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc 
+# sudo apt-key add ros.asc
+# sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
 
 # install development packages
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
+        python-is-python3 \
 		build-essential \
 		cmake \
 		git \
 		libbullet-dev \
 		libpython3-dev \
 		python3-colcon-common-extensions \
+		python3-flake8-docstrings \
 		python3-flake8 \
 		python3-pip \
 		python3-pytest-cov \
@@ -54,33 +66,34 @@ sudo apt-get install -y --no-install-recommends \
 		python3-rosinstall-generator \
 		libasio-dev \
 		libtinyxml2-dev \
-		libcunit1-dev
+		libcunit1-dev \
+		ros-dev-tools
+
 sudo rm -rf /var/lib/apt/lists/*
   
 # install some pip packages needed for testing
 python3 -m pip install -U \
-		argcomplete \
-		flake8-blind-except \
-		flake8-builtins \
-		flake8-class-newline \
-		flake8-comprehensions \
-		flake8-deprecated \
-		flake8-docstrings \
-		flake8-import-order \
-		flake8-quotes \
-		pytest-repeat \
-		pytest-rerunfailures \
-		pytest
+   argcomplete \
+   flake8-blind-except \
+   flake8-builtins \
+   flake8-class-newline \
+   flake8-comprehensions \
+   flake8-deprecated \
+   flake8-import-order \
+   flake8-quotes \
+   "pytest>=5.3" \
+   pytest-repeat \
+   pytest-rerunfailures
 
-# compile yaml-cpp-0.6, which some ROS packages may use (but is not in the 18.04 apt repo)
-git clone --branch yaml-cpp-0.6.0 https://github.com/jbeder/yaml-cpp yaml-cpp-0.6 && \
-    cd yaml-cpp-0.6 && \
+# compile yaml-cpp-0.8, which some ROS packages may use (but is not in the 20.04 apt repo)
+git clone --branch 0.8.0 https://github.com/jbeder/yaml-cpp yaml-cpp-0.8 && \
+    cd yaml-cpp-0.8 && \
     mkdir build && \
     cd build && \
     cmake -DBUILD_SHARED_LIBS=ON .. && \
     make -j$(nproc) && \
-    sudo cp libyaml-cpp.so.0.6.0 /usr/lib/aarch64-linux-gnu/ && \
-    sudo ln -s /usr/lib/aarch64-linux-gnu/libyaml-cpp.so.0.6.0 /usr/lib/aarch64-linux-gnu/libyaml-cpp.so.0.6
+    sudo cp libyaml-cpp.so.0.8.0 /usr/lib/aarch64-linux-gnu/ && \
+    sudo ln -s /usr/lib/aarch64-linux-gnu/libyaml-cpp.so.0.8.0 /usr/lib/aarch64-linux-gnu/libyaml-cpp.so.0.8
 
 
 # https://answers.ros.org/question/325245/minimal-ros2-installation/?answer=325249#post-id-325249
@@ -91,24 +104,25 @@ cat ros2.${ROS_DISTRO}.${ROS_PKG}.rosinstall && \
     vcs import src < ros2.${ROS_DISTRO}.${ROS_PKG}.rosinstall"
 
 # download unreleased packages     
-sudo sh -c "git clone --branch ros2 https://github.com/Kukanani/vision_msgs ${ROS_BUILD_ROOT}/src/vision_msgs && \
-    git clone --branch ${ROS_DISTRO} https://github.com/ros2/demos demos && \
-    cp -r demos/demo_nodes_cpp ${ROS_BUILD_ROOT}/src && \
-    cp -r demos/demo_nodes_py ${ROS_BUILD_ROOT}/src && \
-    rm -r -f demos"
+sudo sh -c "git clone --branch ros2 https://github.com/Kukanani/vision_msgs ${ROS_BUILD_ROOT}/src/vision_msgs 
+#     git clone --branch ${ROS_DISTRO} https://github.com/ros2/demos demos && \
+#     cp -r demos/demo_nodes_cpp ${ROS_BUILD_ROOT}/src && \
+#     cp -r demos/demo_nodes_py ${ROS_BUILD_ROOT}/src && \
+#     rm -r -f demos"
 
 # install dependencies using rosdep
 sudo apt-get update
+sudo apt upgrade
     cd ${ROS_BUILD_ROOT} 
 sudo rosdep init  
     rosdep update && \
-    rosdep install --from-paths src --ignore-src --rosdistro ${ROS_DISTRO} -y --skip-keys "console_bridge fastcdr fastrtps rti-connext-dds-5.3.1 urdfdom_headers qt_gui" && \
+    rosdep install --from-paths src --ignore-src --rosdistro ${ROS_DISTRO} -y --skip-keys "console_bridge fastrtps rfastcdr rti-connext-dds-6.0.1 urdfdom_headers" --os=ubuntu:jammy && \
     sudo rm -rf /var/lib/apt/lists/*
 
 # build it!
 sudo mkdir -p ${ROS_INSTALL_ROOT}
-# sudo required to write build logs
-sudo colcon build --merge-install --install-base ${ROS_INSTALL_ROOT}
+# sudo required to write build logs - try to build the heavy packages first
+sudo colcon build --merge-install --install-base ${ROS_INSTALL_ROOT}  --packages-select rclpy rclcpp
 # We do this twice to make sure everything gets built
 # For some reason, this has been an issue
 sudo colcon build --merge-install --install-base ${ROS_INSTALL_ROOT}
@@ -116,5 +130,4 @@ sudo colcon build --merge-install --install-base ${ROS_INSTALL_ROOT}
 # Using " expands environment variable immediately
 echo "source $ROS_INSTALL_ROOT/setup.bash" >> ~/.bashrc 
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
-echo "export _colcon_cd_root=~/ros2_install" >> ~/.bashrc
-
+# echo "export _colcon_cd_root=~/ros2_install" >> ~/.bashrc
